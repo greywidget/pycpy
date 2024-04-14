2802757c74cbbb8875336492432ef1ea286aa141
Craig Richards <craig@greywidget.co.uk>
2024-04-14 11:57:53 +0100

    Tests passing with default STATIC directory

--- pycpy/builds.py
+++ pycpy/builds.py
@@ -18,27 +18,27 @@ def _process_file(to_file: Path, data: str):
     to_file.write_text(data)
 
 
-def git_ignore(to_dir: Path = Path.cwd()):
+def git_ignore(from_dir: Path = STATIC_DIR, to_dir: Path = Path.cwd()):
     name = ".gitignore"
-    from_file = STATIC_DIR / name
+    from_file = from_dir / name
     data = from_file.read_text()
     to_file = to_dir / name
 
     _process_file(to_file, data)
 
 
-def readme(to_dir: Path = Path.cwd()):
+def readme(from_dir: Path = STATIC_DIR, to_dir: Path = Path.cwd()):
     name = "README.md"
-    from_file = STATIC_DIR / name
+    from_file = from_dir / name
     data = from_file.read_text()
     to_file = to_dir / name
 
     _process_file(to_file, data)
 
 
-def pyproject(to_dir: Path = Path.cwd()):
+def pyproject(from_dir: Path = STATIC_DIR, to_dir: Path = Path.cwd()):
     name = "pyproject.toml"
-    from_file = STATIC_DIR / name
+    from_file = from_dir / name
     data = from_file.read_text()
     data = data.replace(PLACEHOLDER_NAME, PROJECT_NAME)
     to_file = to_dir / name
